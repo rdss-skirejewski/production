53ac4db880e074e87ce943fcbe8da6073b583dd4
rdss-skirejewski <sebastian.kirejewski@rewe-digital.com>
2021-07-08 14:34:58 +0200

    FIX field

--- custom/plugins/PennyTestPlugin/src/Extension/Content/Product/PennyProductExtension.php
+++ custom/plugins/PennyTestPlugin/src/Extension/Content/Product/PennyProductExtension.php
@@ -6,7 +6,7 @@ namespace PennyTestPlugin\Extension\Content\Product;
 
 use Shopware\Core\Content\Product\ProductDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityExtension;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Runtime;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 
@@ -15,7 +15,8 @@ class PennyProductExtension extends EntityExtension
     public function extendFields(FieldCollection $collection): void
     {
         $collection->add(
-            new StringField('penny_custom_field', 'pennyCustomField')
+            (new StringField('penny_custom_field', 'pennyCustomField'))
+                ->addFlags(new Runtime())
         );
     }
 
