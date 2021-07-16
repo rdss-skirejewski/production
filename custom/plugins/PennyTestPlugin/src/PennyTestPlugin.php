fa255c44f58cde03dc44c54788f2b7f3edec054f
rdss-akuehn <alex.kuehn@rewe-digital.com>
2021-07-16 16:01:34 +0200

    Try to extend storefront theme

--- custom/plugins/PennyTestPlugin/src/PennyTestPlugin.php
+++ custom/plugins/PennyTestPlugin/src/PennyTestPlugin.php
@@ -3,11 +3,17 @@
 namespace PennyTestPlugin;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
 
-class PennyTestPlugin extends Plugin
+class PennyTestPlugin extends Plugin implements ThemeInterface
 {
     public function getMigrationNamespace(): string
     {
         return 'PennyTestPlugin\Migration';
+    }
+
+    public function getThemeConfigPath(): string
+    {
+        return 'theme.json';
     }
 }
